e75ec008b06da5e1e6ea6067c2c4126505f88a45
Justin Tuttle <reddhouse@gmail.com>
2016-02-25 21:41:32 -0800

    Add sparklines-react library + WeatherList mods to app

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+			// Not mutating, rather returning a new instance
+			// of state via ES6 style concatenation
+			return [ action.payload.data, ...state ];
+	}
 	return state;
 }
