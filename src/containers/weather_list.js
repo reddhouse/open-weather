696283bd33f9f9a42860290e7df953ac3c7d32ce
Justin Tuttle <reddhouse@gmail.com>
2016-02-27 16:34:55 -0800

    Add Google Maps feature, round out app though lecture 67

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
-		const temps = cityData.list.map(weather => weather.main.temp);
-		console.log(temps);
+		// const temps = cityData.list.map(weather => weather.main.temp);
+		// upgraded to fahrenheit
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), 
+			(temp) => (temp - 273)*1.8 + 32);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		// const lon = cityData.city.coord.lon;
+		// const lat = cityData.city.coord.lat;
+		// ES6 refactor with "destructuring"
+		const { lon, lat } = cityData.city.coord;
 
 		return (
 			<tr key={name}>
-				<td>{name}</td>
-				<td>
-					<Sparklines height={120} width={180} data={temps}>
-						<SparklinesLine color="red" />
-					</Sparklines>
-				</td> 
+				<td><GoogleMap lon={lon} lat={lat} /></td>
+				<td><Chart data={temps} color="orange" units="K" /></td>
+				<td><Chart data={pressures} color="green" units="hPa" /></td> 
+				<td><Chart data={humidities} color="black" units="%" /></td> 
 			</tr>
 		);
 	}
@@ -27,9 +34,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (F)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
